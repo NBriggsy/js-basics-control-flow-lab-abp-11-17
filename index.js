a39ffc7c17c7a243779d3b5b58e09b04f04f77ec
NBriggsy <hello.briggs1@gmail.com>
2017-11-13 03:46:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,5 @@ function ternaryCheckCity (city) {
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip (tip) {
-switch (tip) {
-  case 'generous':
-    return = 'Thank you so much.';
-    break;
-}
+
 
